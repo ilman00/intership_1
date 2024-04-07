142f7a4cd5e8257d8014921040eb7fbebd36e63a
ILMAN <ilmanalisher896@gmail.com>
2024-04-07 13:03:42 +0500

    Update app.js and add_designation.ejs

--- app.js
+++ app.js
@@ -6,12 +6,43 @@ const dbConnection = require("./db");
 
 const app = express();
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
+
+const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS designation (
+        title VARCHAR(255) NOT NULL,
+    )
+`;
+
+dbConnection.query(createTableQuery, (error, results) => {
+    if (error) {
+        console.error('Error creating table: ', error);
+        return;
+    }
+    console.log('Table created successfully.');
+});
+
+app.get("/add_designation", (req, res) => {
     res.render("add_designation")
 });
+
+app.post("/add_designation", (req, res)=>{
+    const addDesignation = req.body.designation;
+    const data = `INSERT INTO designation (title) VALUES (?)`
+    dbConnection.query(data, addDesignation, (error, results, fields)=>{
+        if(error){
+            console.log("Error inserting data ", error);
+            return;
+        }
+        console.log("data inserted");
+    });
+    
+});
+
+
 
 app.get("/add_role", (req, res)=>{
     res.render("add_role");
