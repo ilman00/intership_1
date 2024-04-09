3cb16bcc302d613befa0a2672a7decae284353d5
ILMAN <ilmanalisher896@gmail.com>
2024-04-09 13:01:05 +0500

    changed 1:00PM 4/9/24

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const multer = require("multer");
 const dbConnection = require("./db");
+const path = require("path");
 
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }))
-
-
-// const createTableQuery = `
-// CREATE TABLE your_table_name (
-//     id INT AUTO_INCREMENT PRIMARY KEY,
-//     title VARCHAR(255),
-//     timeStamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-// )`;
-
-// dbConnection.query(createTableQuery, (error, results) => {
-//     if (error) {
-//         console.error('Error creating table: ', error);
-//         return;
-//     }
-//     console.log('Table created successfully.');
-// });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// multer
+var storage = multer.diskStorage({
+    destination: (req, file, callBack) => {
+      callBack(null, './uploads/')     // './uploads/' directory name where save the file
+    },
+    filename: (req, file, callBack) => {
+      callBack(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+  })
+  
+  var upload = multer({
+    storage: storage
+  });
 
 
 
@@ -69,7 +69,7 @@ app.get("/add_role", (req, res) => {
 
 });
 
-app.post("/add_role", (req, res)=> {
+app.post("/add_role", (req, res) => {
     const addRole = req.body.role;
     console.log(addRole);
     const data = `INSERT INTO role (roleTitle) VALUES (?)`;
@@ -81,18 +81,55 @@ app.post("/add_role", (req, res)=> {
         console.log("data inserted");
         res.redirect("/add_role");
     });
-} );
-
-// user rout
-app.get("/add_user", (req, res)=>{
-    res.render("add_user");
 });
 
-app.post("/add_user", (req, res)=>{})
+
+
+// user rout
+app.get("/add_user", (req, res) => {
+    const myquery = `SELECT * FROM user`;
+    dbConnection.query(myquery, (err, results, fields)=>{
+        console.log(results);
+        res.render("add_user", {userData: results});
+    })
+
+});
+
+app.post("/add_user", upload.single('image'), (req, res) => { 
+    const role = req.body.role;
+    const designation = req.body.designation;
+    const name = req.body.name;
+    const email = req.body.email;
+    const mobile = req.body.mobile;
+    const password = req.body.password;
+    const organization = req.body.organization;
+    const department = req.body.department;
+
+    console.log("req.file above the if condition :", req.file.filename );
+    if (!req.file) {
+        console.log("No file upload");
+      } else {
+        console.log(req.file.filename)
+        var imgsrc = "/uploads/" + req.file.filename
+        var queryArr = [role, designation, name, email, mobile, password, organization, department, imgsrc]
+        var insertData = "INSERT INTO user(role, designation, name, email, mobile, password, organization, department, imagePath)VALUES(?,?,?,?,?,?,?,?,?)";
+        dbConnection.query(insertData, queryArr, (err, result) => {
+          if (err) throw err
+          console.log("file uploaded");
+    
+          res.redirect("/add_user");
+        });
+      }
+
+})
+
+
+
+
 
 
 // organization rout
-app.get("/add_organization", (req, res)=>{
+app.get("/add_organization", (req, res) => {
     const requestOrg = `SELECT * FROM organization`;
 
     dbConnection.query(requestOrg, (err, results, fields) => {
@@ -102,7 +139,7 @@ app.get("/add_organization", (req, res)=>{
     });
 });
 
-app.post("/add_organization", (req, res)=>{
+app.post("/add_organization", (req, res) => {
     const addOrg = req.body.org;
     console.log(addOrg);
     const data = `INSERT INTO organization (orgTitle) VALUES (?)`;
@@ -118,21 +155,21 @@ app.post("/add_organization", (req, res)=>{
 
 
 // sub Department rout
-app.get("/add_subdepartment", (req, res)=>{
+app.get("/add_subdepartment", (req, res) => {
     const requestOrg = `SELECT orgTitle FROM organization`;
     const requestDepartment = `SELECT * FROM department`;
 
-    dbConnection.query(requestOrg, (err1, resultOrg, fields1)=>{
-        dbConnection.query(requestDepartment, (err2, resultDep, fields2 )=>{
+    dbConnection.query(requestOrg, (err1, resultOrg, fields1) => {
+        dbConnection.query(requestDepartment, (err2, resultDep, fields2) => {
 
             res.render("add_subdepartment", { resultOrg: resultOrg, resultDep: resultDep });
-        
+
         });
     });
 
 });
 
-app.post("/add_subdepartment", (req, res)=>{
+app.post("/add_subdepartment", (req, res) => {
     const addDep = req.body.department;
     const addOrg = req.body.organization;
     var depArray = [addOrg, addDep]
@@ -148,8 +185,41 @@ app.post("/add_subdepartment", (req, res)=>{
     });
 
 
+});
+
+app.get("/edit/:file", (req, res) => {
+    console.log(req.params.file);
+    const param = req.params.file;
+    
+    res.render("edit");
 })
 
+
+app.post("/edit/:file", (req, res) => {
+    const param = req.params.file;
+    console.log( "post : ", param);
+
+    const tableName = param.split(/[^a-zA-Z]/)[0];
+    const id = param.split(/[^0-9]/).pop();
+    console.log("table Name: "+ tableName, "table Id:" + id);
+    const newTitle = req.body.title;
+    if(tableName === "designation"){
+        var sql = `UPDATE ${tableName} SET title = '${newTitle}' WHERE id = ${id}`;
+    }else if(tableName === "department"){
+        var sql = `UPDATE ${tableName} SET depTitle = '${newTitle}' WHERE depId = ${id}`;
+    }else if(tableName === "organization"){
+        var sql = `UPDATE ${tableName} SET orgTitle = '${newTitle}' WHERE orgId = ${id}`;
+    }else if(tableName === "role"){
+        var sql = `UPDATE ${tableName} SET roleTitle = '${newTitle}' WHERE roleId = ${id}`;
+    }
+
+    dbConnection.query(sql, (err, results)=>{
+        if(err) throw err;
+        res.redirect("/add_designation");
+    });
+
+
+})
 
 
 app.listen(5000, () => {
